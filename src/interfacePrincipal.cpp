bae2f55a6540824655ef83db9ce6d627b418b505
Kleidson <kleidsonalves15@gmail.com>
2020-08-28 18:58:24 -0300

    Menu recorrente

--- src/interfacePrincipal.cpp
+++ src/interfacePrincipal.cpp
@@ -54,43 +54,68 @@ void InterfacePrincipal::menuFazerLogin() {
 }
 
 void InterfacePrincipal::menuPrincipal() {
-    system("clear"); 
-    int opcao; 
-    cout << "(1) Amigos" << endl;
-    cout << "(2) Grupos de estudo" << endl; 
-    cout << "(3) Gerenciar perfil" << endl;
-    cout << "(4) Sair da conta" << endl; 
-    opcao = getInt(); 
-    system("clear"); 
+    int paraLoop = 0; 
+    while (true){
+        system("clear"); 
+        int opcao; 
+        cout << "(1) Amigos" << endl;
+        cout << "(2) Grupos de estudo" << endl; 
+        cout << "(3) Gerenciar perfil" << endl;
+        cout << "(4) Sair da conta" << endl; 
+        opcao = getInt(); 
 
-    switch (opcao){
-        case 1:{
-            menuAmigos();     
-            break;
+        switch (opcao){
+            case 1:{
+                menuAmigos();     
+                break;
+            }
+            case 2: {
+                menuGrupos();  
+                break;
+            }
+            case 3: {
+                menuConfiguracao();  
+                break;
+            }
+            default:
+                paraLoop = 1;
+                break; 
         }
-        case 2: {
-            menuGrupos();  
-            break;
-        }
-        case 3: {
-            menuConfiguracao();  
-            break;
-        }
-        default:
+        if(paraLoop == 1)
             break; 
     }
 }
 
-    void InterfacePrincipal::menuAmigos(){
-        int opcao; 
-        cout << "(1) Meus amigos" << endl;
-        cout << "(2) Adicionar amigo" << endl; 
-        cout << "(3) Voltar" << endl; 
-        opcao = getInt(); 
+void InterfacePrincipal::menuAmigos(){
+    int opcao; 
+    cout << "(1) Meus amigos" << endl;
+    cout << "(2) Adicionar amigo" << endl; 
+    cout << "(3) Voltar" << endl; 
+    opcao = getInt(); 
+
+    if(opcao == 1){
+
     }
-    void InterfacePrincipal::menuGrupos(){
-       
-    }  
-    void InterfacePrincipal::menuConfiguracao(){
-        
+    else if(opcao == 3){
+
     }
+    else{
+
+    }
+}
+
+void InterfacePrincipal::menuGrupos(){
+    int opcao; 
+    cout << "(1) Meus grupos" << endl;
+    cout << "(2) Criar grupo" << endl; 
+    cout << "(3) Voltar" << endl; 
+    opcao = getInt(); 
+}
+
+void InterfacePrincipal::menuConfiguracao(){
+    int opcao; 
+    cout << "(1) Atualizar dados" << endl;
+    cout << "(2) Excluir conta" << endl; 
+    cout << "(3) Voltar" << endl; 
+    opcao = getInt(); 
+}
